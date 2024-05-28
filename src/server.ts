111f422eba6b43aa8e97c35a217e49e8a485f75d
sadek1999 <sadekali1530@gmail.com>
2024-05-28 20:17:36 +0600

    connect mongodb atlas successfully

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+let port=3000;
+
+
+
+async function main() {
+    await mongoose.connect(config.db_url as string);
+    
+    
+    app.listen(config.port, () => {
+        console.log(`Example app listening on port ${config.port}`)
+      })
+  }
+
+ main()
