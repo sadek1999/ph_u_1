1456ef79979d620938ecafdd02787e0bb49153c1
sadek1999 <sadekali1530@gmail.com>
2024-07-16 14:42:53 +0600

    set offeredcourse interface and model

---
+++ src/app/modules/offeredCourse/offeredCourse.interface.ts
@@ -0,0 +1,18 @@
+import { Types } from "mongoose"
+
+export type TDays = 'Sat' | 'Sun' | 'Mon' | 'Tue' | 'Wed' | 'Thu' | 'Fri';
+
+export type TOfferedCourse={
+    semesterRegistration:Types.ObjectId,
+    academicDepartment:Types.ObjectId;
+    academicFaculty:Types.ObjectId;
+    academicSemester:Types.ObjectId;
+    course:Types.ObjectId;
+    Faculty:Types.ObjectId;
+    maxCapacity:number;
+    section:number;
+    days:TDays[];
+    startTime:string;
+    endTime:string
+
+}
